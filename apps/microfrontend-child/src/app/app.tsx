ab0902ecdb211b455c92760d494f4f222a2f61f4
Finlay Smith <finlay.smith@ringcentral.com>
2023-12-13 18:09:30 +0000

    fix(microfrontend-child): testing independent versioning

--- apps/microfrontend-child/src/app/app.tsx
+++ apps/microfrontend-child/src/app/app.tsx
@@ -3,8 +3,8 @@ import styles from './app.module.css';
 export function App() {
   return (
     <div className={styles.container}>
-      Microfrontend Child! <br /> I have a completely separate build process,
-      and am deployed independently of the app shell
+      <div>Microfrontend Child!</div>
+      <div>I am deployed independently of the app shell</div>
     </div>
   );
 }
